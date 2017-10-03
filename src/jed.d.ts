5f1466c7acc6340f3662a15606dd7364e27202f4
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2017-10-03 06:28:35 -0500

    tslint without project

--- src/jed.d.ts
+++ src/jed.d.ts
@@ -1,1 +1,1 @@
-declare module 'jed';
+declare module "jed";
--- src/pure-component.tsx
+++ src/pure-component.tsx
@@ -2,40 +2,40 @@ import * as Jed from "jed";
 import * as React from "react";
 
 export interface IGettextProp {
-	languageCode: string;
-	languageLoadedCallback?: (err?: Error) => void;
+  languageCode: string;
+  languageLoadedCallback?(err?: Error): void;
 }
 
 export abstract class JedPureComponent<P, S> extends React.PureComponent<IGettextProp, S> {
-	private i18n = new Jed({locale_data: { messages: { "": {} } } });
-	props: Readonly<{ children?: React.ReactNode }> & Readonly<IGettextProp> & Readonly<P>;
+  public props: Readonly<{ children?: React.ReactNode }> & Readonly<IGettextProp> & Readonly<P>;
+  private i18n = new Jed({locale_data: { messages: { "": {} } } });
+  // tslint:disable-next-line no-any
+  private constructor(props?: IGettextProp & P, context?: any) {
+    super(props, context);
+    this.initLanguage();
+  }
+  protected _(msgid: string): string {
+    return this.i18n.gettext.apply(this.i18n, arguments);
+  }
+  protected abstract async getTranslation(): Promise<{}>;
+  protected ngettext(msgid: string, msgidPlural: string, value: number): string {
+    return this.i18n.ngettext.apply(this.i18n, arguments);
+  }
 
-	constructor(props?: IGettextProp & P, context?: any) {
-		super(props, context);
-		this.props
-		this.initLanguage();
-	}
-	protected abstract async getTranslation(): Promise<void>;
-	protected _(msgid: string): string {
-		return this.i18n.gettext.apply(this.i18n, arguments);
-	}
-	protected ngettext(msgid: string, msgid_plural: string, value: number): string {
-		return this.i18n.ngettext.apply(this.i18n, arguments);
-	}
-
-	private async initLanguage() {
-		const languageLoadedCallback = this.props.languageLoadedCallback || ((err?: Error) => {
-			if (err && console) {
-				console.warn(err)
-			}
-		});
-		try {
-			const l = await this.getTranslation();
-			this.i18n = new Jed(l);
-			this.forceUpdate();
-			languageLoadedCallback();
-		} catch (e) {
-			languageLoadedCallback(e);
-		}
-	}
+  private async initLanguage() {
+    const languageLoadedCallback = this.props.languageLoadedCallback || ((err?: Error) => {
+      if (err && console) {
+        // tslint:disable-next-line no-console
+        console.warn(err);
+      }
+    });
+    try {
+      const l = await this.getTranslation();
+      this.i18n = new Jed(l);
+      this.forceUpdate();
+      languageLoadedCallback();
+    } catch (e) {
+      languageLoadedCallback(e);
+    }
+  }
 }
